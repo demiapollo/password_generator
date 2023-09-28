3adced1b4a32c53ad29f9bb3411b8751e6fa1a13
demiapollo <apollodemirel67@gmail.com>
2023-09-27 21:39:13 -0700

    password generator created a random password based on user input of desired number of letters, symbols, and numbers

---
+++ password.py
@@ -0,0 +1,27 @@
+# Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+print("------------------------------------\n")
+nr_letters = int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Choosing random letter using nr_letters
+letters_selected = random.choices(letters, k=nr_letters)
+symbols_selected = random.choices(symbols, k=nr_symbols)
+numbers_selected = random.choices(numbers, k=nr_numbers)
+
+combined_selected = letters_selected + symbols_selected + numbers_selected
+
+# Randomize the order of characters in the list
+random.shuffle(combined_selected)
+
+password = ""
+for character in combined_selected:
+    password += character
+print(f"Here is your password: {password}")
+
